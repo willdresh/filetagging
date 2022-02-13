150fbe1198a9044cdc9f5ead989ca21d1a3a0095
Ragnar Danneskjold <real_ragnardanneskjold@pm.me>
2022-02-12 18:10:49 -0600

    minor: pulled part of addtag into a separate file called aux_addtag.py; did not modify code

---
+++ aux_addtag.py
@@ -0,0 +1,22 @@
+
+
+def printHelpText():
+    print('Syntax:\taddtag [-v] <filename> <tag> [additional-tag] ... [additional-tagN]\n')
+
+def reportTagAlreadyApplied(exc):
+    print('\tNon-Fatal Exception: requested tag already applied');
+    print('\tContinuing...');
+
+# The following "printXtoY" methods are only called when running in verbose mode
+def printAddDirToDBMessage(target_dir):
+    print(f"Adding new directory {target_dir} to database");
+
+def printAddFileToDBMessage(target):
+    print(f"Adding new file {target} to database")
+
+def printAddTagToDBMessage(tagName):
+    print(f"Adding new tag {tagName} to database")
+
+def printAddTagToFileMessage(tagId, target):
+    print(f"Applying tag {tagId} to file {target}")
+
